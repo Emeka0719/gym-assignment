db6621a160b9b6f8aff735a713bbd367f7e1ba5f
Emeka <emekaedeoga@gmail.com>
2024-08-07 12:28:05 +0100

    Gym assignment

---
+++ wednesday.js
@@ -0,0 +1,19 @@
+// alert("Are you sure you want to exit");
+// let name = Emeka;
+// alert("My name is"+ name);
+// let ffood = Rice;
+// alert(`my fav food is ${ffood}`);
+
+// prompt("What is your favorite holiday?");
+// let holiday = prompt("what is your favorite holiday?");
+// alert(`you answered ${holiday}`);
+
+//  let a = parseInt(prompt("enter a number"));
+// let b = parseInt(prompt("enter a another number"));
+// let c = a + b;
+// alert(`the sum is ${c}`);
+
+let W = parseFloat(prompt("enter your body weight"));
+let H = parseFloat(prompt("enter your  Height"));
+let BMI =  W/(H*H)
+alert(`your bmi is ${BMI}`)
